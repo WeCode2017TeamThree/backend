97c9af5d99ff1cd21857e75c41ff04c84244d478
Shannon Mulloy <smulloy21@gmail.com>
2017-12-02 12:11:37 -0800

    try this instead

--- glyphs/const.py
+++ glyphs/const.py
@@ -5,6 +5,6 @@ import os
 # mongo connect strings
 GLYPHS_DB = 'glyphs'
 DB_ALIAS_DEFAULT = 'default'
-MONGO_HOST = os.environ.get('MONGODB_URI', 'localhost')
+MONGO_HOST = os.environ.get('MONGO_URL', 'localhost')
 _DB_CONNECT_STRING = 'mongodb://%s/' % MONGO_HOST
 GLYPHS_DB_CONNECT = _DB_CONNECT_STRING + GLYPHS_DB
